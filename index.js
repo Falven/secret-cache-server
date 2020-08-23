0062e434a30e8bacd2fd2c1a65a345157d1541aa
Francisco Aguilera <falven2000@hotmail.com>
2020-08-23 18:12:42 -0400

    Added environment values and web.config.

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ const EventDrivenSecretCache = require('../secret-cache');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-process.env['AZURE_KEYVAULT_NAME'] = 't-akv-2n-searchpage-01';
-process.env['AZURE_CLIENT_ID'] = '';
-process.env['AZURE_CLIENT_SECRET'] = '';
-process.env['AZURE_TENANT_ID'] = '';
+process.env['AZURE_KEYVAULT_NAME'] = 'kv-secret-cache';
+process.env['AZURE_CLIENT_ID'] = '55d7e915-8dda-45f9-b67f-69222d7264fd';
+process.env['AZURE_CLIENT_SECRET'] = '327491ae-b4f2-4da7-9b39-e360562a9d1b';
+process.env['AZURE_TENANT_ID'] = '215fd765-750e-4adf-8bb7-7a009994dde6';
 
 const server = http.createServer(async (req, res) => {
   var cache = new EventDrivenSecretCache();
   await cache.init();
 
-  const json = { message: 'hello azure.' }
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(json));
+  const json = cache.secrets['ServiceDiscoveryData'];
+  res.end(json);
 });
 
 server.listen(port, hostname, () => {
