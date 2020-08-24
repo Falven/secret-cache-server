2c5ac188dbb03b267fd77d9b6b616226e8e49c29
Francisco Aguilera <falven2000@hotmail.com>
2020-08-24 17:56:42 -0400

    Cleaned up and moved code to module.

--- server.js
+++ server.js
@@ -21,42 +21,6 @@ server.get("/", (_req, res) => {
   res.status(200).set('Content-Type', 'application/json').end(JSON.stringify(cache.secrets));
 });
 
-server.use(bodyParser.json());
-
-server.post("/api/updates", async (req, res) => {
-  console.log('Received Event.');
-  console.log('Headers:\n' + JSON.stringify(req.headers));
-  console.log('Body:\n' + JSON.stringify(req.body));
-
-  var header = req.get("Aeg-Event-Type");
-  if (header && req.body && Object.keys(req.body).length > 0) {
-    console.log('Header exists.');
-    var event = req.body[0];
-
-    if (event && event.eventType && event.data) {
-      console.log('Body exists.');
-      // Check for Webhook validation handshake event type.
-      if (header === 'SubscriptionValidation') {
-        console.log('SubscriptionValidation event.');
-        if (event.data.validationCode && event.eventType == 'Microsoft.EventGrid.SubscriptionValidationEvent') {
-          console.log('Microsoft.EventGrid.SubscriptionValidationEvent');
-          return res.send({"validationResponse": event.data.validationCode})
-        }
-      }
-
-      // Check for KeyVault secret event type.
-      if (header === 'Notification') {
-        console.log('Notification event.');
-        if (event.eventType == 'Microsoft.KeyVault.SecretNewVersionCreated') {
-          console.log('Microsoft.KeyVault.SecretNewVersionCreated');
-          await cache.updateSecret(event.data.ObjectName);
-          return res.status(200).end();
-        }
-      }
-    }
-  }
-});
-
 server.listen(port, async () => {
   await cache.init();
   console.log(`🚀 Server running on port ${port}`);
