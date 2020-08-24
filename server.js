57fe4d18c374ac441cef88665dcc8bf039638fce
Francisco Aguilera <falven2000@hotmail.com>
2020-08-24 10:22:44 -0400

    Simpler node app for testing.

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ process.env['AZURE_CLIENT_SECRET'] = '327491ae-b4f2-4da7-9b39-e360562a9d1b';
 process.env['AZURE_TENANT_ID'] = '215fd765-750e-4adf-8bb7-7a009994dde6';
 
 const server = http.createServer(async (req, res) => {
-  var cache = new EventDrivenSecretCache();
-  await cache.init();
+  // var cache = new EventDrivenSecretCache();
+  // await cache.init();
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  const json = cache.secrets['ServiceDiscoveryData'];
-  res.end(json);
+  // const json = cache.secrets['ServiceDiscoveryData'];
+  res.end('{ hello: "world" }');
 });
 
 const port = process.env.PORT || 3000;
