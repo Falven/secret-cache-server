4273b84ded0379178b0055ff5ae83e4cc2126c4d
Francisco Aguilera <falven2000@hotmail.com>
2020-08-24 12:30:19 -0400

    Using express.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * license information.
  */
 
-const http = require('http');
+const express = require("express");
 const EventDrivenSecretCache = require('@mcs/secret-cache');
 
 process.env['AZURE_KEYVAULT_NAME'] = 'kv-secret-cache';
@@ -12,17 +12,14 @@ process.env['AZURE_CLIENT_ID'] = '55d7e915-8dda-45f9-b67f-69222d7264fd';
 process.env['AZURE_CLIENT_SECRET'] = '327491ae-b4f2-4da7-9b39-e360562a9d1b';
 process.env['AZURE_TENANT_ID'] = '215fd765-750e-4adf-8bb7-7a009994dde6';
 
-const server = http.createServer(async (req, res) => {
-  var cache = new EventDrivenSecretCache();
-  await cache.init();
-
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(cache.secrets));
-});
-
+const server = express();
 const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log(`Server running at http://${process.env.HOST}:${port}/`);
+server.listen(port, () => console.log(`🚀 Server running on port ${port}`));
+
+server.get("/", async (req, res) => {
+  var cache = new EventDrivenSecretCache(server);
+  await cache.init();
+
+  res.status(200).set('Content-Type', 'application/json').end(JSON.stringify(cache.secrets));
 });
