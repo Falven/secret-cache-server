678ec0a2664f74ac9796c5c68f7cdf30831c5e90
Francisco Aguilera <falven2000@hotmail.com>
2020-08-25 11:54:47 -0400

    Revert "Merge branch 'master' of https://github.com/Falven/secret-cache-server into master"
    This reverts commit b32e744492533e58c350674cd819a5792e5548bb, reversing
    changes made to 513ca80bd197a997b12919ce3a090befb3ca59e6.

--- server.js
+++ server.js
@@ -4,26 +4,23 @@
  * license information.
  */
 
-const http = require('http');
+const express = require('express');
 const EventDrivenSecretCache = require('@mcs/secret-cache');
 
-process.env['AZURE_KEYVAULT_NAME'] = '';
-process.env['AZURE_CLIENT_ID'] = '';
-process.env['AZURE_CLIENT_SECRET'] = '';
-process.env['AZURE_TENANT_ID'] = '';
+process.env['AZURE_KEYVAULT_NAME'] = 'kv-secret-cache';
+process.env['AZURE_CLIENT_ID'] = '55d7e915-8dda-45f9-b67f-69222d7264fd';
+process.env['AZURE_CLIENT_SECRET'] = '327491ae-b4f2-4da7-9b39-e360562a9d1b';
+process.env['AZURE_TENANT_ID'] = '215fd765-750e-4adf-8bb7-7a009994dde6';
 
-const server = http.createServer(async (req, res) => {
-  var cache = new EventDrivenSecretCache();
-  await cache.init();
+const server = express();
+const port = process.env.PORT || 3000;
+const cache = new EventDrivenSecretCache(server);
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  const json = cache.secrets['ServiceDiscoveryData'];
-  res.end(json);
+server.get("/", (_req, res) => {
+  res.status(200).set('Content-Type', 'application/json').end(JSON.stringify(cache.secrets));
 });
 
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`Server running at http://${process.env.HOST}:${port}/`);
+server.listen(port, async () => {
+  await cache.init();
+  console.log(`🚀 Server running on port ${port}!`);
 });
